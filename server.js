2606d62976b51d1844f6d83c97382987938270fe
Victor Anchondo <eduardoanchondo@gmail.com>
2022-07-27 11:44:12 -0600

    Updating deploy configurations

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static('./dist/ui-tfm'));
-
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/ui-tfm/'});
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
