ab05f41385f8498859bb05831b633b5afbdfabe0
Victor Anchondo <eduardoanchondo@gmail.com>
2023-01-12 21:48:29 -0600

    Adding commands to upload docker image

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-docker image build -t ui-tfm:latest .
-docker stop ui-tfm
-docker rm ui-tfm
-docker run --name ui-tfm -d --restart unless-stopped -p80:80 ui-tfm:latest 
+IMAGE=ui-tfm
+
+# Creates docker image
+docker image build -t $IMAGE:latest .
+
+# Upload image to jfrog
+docker tag $IMAGE vanchondo.jfrog.io/tfm/$IMAGE
+docker push vanchondo.jfrog.io/tfm/$IMAGE
+
+# Stop, remove and start docker container
+docker stop $IMAGE
+docker rm $IMAGE
+docker run --name $IMAGE -d --restart unless-stopped -p80:80 $IMAGE:latest 
